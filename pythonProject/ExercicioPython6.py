ebc4da1a4584a7c198838f2d54059bb327b22c9e
Mariana Souza <148583625+DevMarianaSouza@users.noreply.github.com>
2024-04-21 20:58:24 -0300

    Delete pythonProject directory

--- pythonProject/ExercicioPython6.py
+++
@@ -1,4 +0,0 @@
-km = float(input('Quantos quilômetros você percorreu?'))
-dias = int(input('Quantos dias você alugou esse carro?'))
-preço = (dias * 60) + (km * 0.15)
-print('Se você alugou por {} dias e percorreu {:.2f}Km, você pagará o valor de R${:.2f}.'.format(dias, km, preço))
